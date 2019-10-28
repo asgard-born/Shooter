b75ffe61e25a90da477a2fd060e0dc8a0091c8c1
Victor <unicaxverdad@gmail.com>
2019-10-28 17:52:16 +0300

    added IK, refactor

--- Assets/Code/Controllers/MovementController.cs
+++ Assets/Code/Controllers/MovementController.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using RootMotion.FinalIK;
 
 namespace Controllers {
+    using System;
+    using System.Threading.Tasks;
     using UnityEngine;
 
     public class MovementController : MonoBehaviour {
         private Animator animator;
 
         // basic motion variables
-        private                  float forwardMoving;
-        private                  float horizontalMoving;
-        private                  float run;
-        private                  bool  sneak;
-        [SerializeField] private float movingSpeed;
+        private float forwardMoving;
+        private float horizontalMoving;
+        private float run;
+        private bool  sneak;
 
-        private float normalizedSpeed;
+        [SerializeField] private float movingSpeed;
 
         // jumping variables
         private       bool  isJumping;
@@ -25,28 +25,29 @@ namespace Controllers {
 
         private float     mouseX;
         private Rigidbody rigidbody;
-        private Weapon    weapon;
         private float     turnSmoothVelocity;
 
-        private static readonly int Grounded        = Animator.StringToHash("Grounded");
-        private static readonly int Speed           = Animator.StringToHash("Speed");
-        private static readonly int NormalizedSpeed = Animator.StringToHash("NormalizedSpeed");
-        private static readonly int Side            = Animator.StringToHash("Side");
-        private static readonly int Sneak           = Animator.StringToHash("Sneak");
-        private static readonly int Jump            = Animator.StringToHash("Jump");
-        private static readonly int Falling         = Animator.StringToHash("Falling");
-        private static readonly int NearGround      = Animator.StringToHash("NearGround");
+        private WeaponController weapon;
+        private AimIK aimIk;
 
-        public bool getSneak => this.sneak;
+        private static readonly int Grounded   = Animator.StringToHash("Grounded");
+        private static readonly int Speed      = Animator.StringToHash("Speed");
+        private static readonly int Side       = Animator.StringToHash("Side");
+        private static readonly int Sneak      = Animator.StringToHash("Sneak");
+        private static readonly int Jump       = Animator.StringToHash("Jump");
+        private static readonly int Falling    = Animator.StringToHash("Falling");
+        private static readonly int NearGround = Animator.StringToHash("NearGround");
 
-        public float getRun => this.run;
-
-        public bool getGroundedMarker => this.IsGrounded();
+        public bool  getSneak          => this.sneak;
+        public float getRun            => this.run;
+        public bool  getGroundedMarker => this.IsGrounded();
 
         private void Start() {
-            this.animator  = this.GetComponent<Animator>();
-            this.rigidbody = this.GetComponent<Rigidbody>();
-            this.weapon    = this.GetComponent<Weapon>();
+            this.animator                                      = this.GetComponent<Animator>();
+            this.rigidbody                                     = this.GetComponent<Rigidbody>();
+            this.weapon                                        = this.GetComponent<WeaponController>();
+            this.aimIk = this.GetComponent<AimIK>();
+            this.aimIk.solver.IKPositionWeight = 1;
         }
 
         private void Update() {
@@ -54,7 +55,14 @@ namespace Controllers {
             this.horizontalMoving =  Input.GetAxis("Horizontal");
             this.sneak            =  this.IsGrounded() && (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl));
             this.mouseX           += Input.GetAxis("Mouse X");
-            this.run              =  Input.GetButton("Run") && this.IsGrounded() ? 1 : 0;
+            this.run              =  Input.GetButton("Run") && !this.sneak && this.IsGrounded() ? 1 : 0;
+
+            if (this.run == 1) {
+                this.aimIk.solver.IKPositionWeight = 0;
+            }
+            else {
+                this.aimIk.solver.IKPositionWeight = 1;
+            }
 
             this.forwardMoving    += this.forwardMoving    > 0 ? this.run : this.forwardMoving < 0 ? -this.run : 0;
             this.horizontalMoving += this.horizontalMoving > 0 ? this.run : this.horizontalMoving < 0 ? -this.run : 0;
@@ -69,13 +77,12 @@ namespace Controllers {
             this.rigidbody.MovePosition(this.transform.position + (this.transform.right * this.horizontalMoving + this.transform.forward * this.forwardMoving));
         }
 
-        private async void LateUpdate() {
+        private void LateUpdate() {
             this.animator.SetFloat(Speed, this.forwardMoving);
-            this.animator.SetFloat(NormalizedSpeed, this.normalizedSpeed);
             this.animator.SetFloat(Side, this.horizontalMoving);
             this.animator.SetBool(Sneak, this.sneak);
             this.animator.SetBool(Jump, this.isJumping);
-            this.animator.SetBool(Falling, this.isFalling());
+            this.animator.SetBool(Falling, this.IsFalling());
             this.animator.SetBool(Grounded, this.IsGrounded());
             this.animator.SetBool(NearGround, this.IsNearToGround());
 
@@ -84,14 +91,12 @@ namespace Controllers {
                 this.isJumping = true;
             }
 
-            Debug.Log("isFalling " + this.isFalling());
-
-            if (this.isFalling() && this.IsNearToGround()) {
-                await this.UpdateJumpChecker();
+            if (this.IsFalling() && this.IsNearToGround()) {
+                this.UpdateJumpChecker();
             }
         }
 
-        private async Task UpdateJumpChecker() {
+        private async void UpdateJumpChecker() {
             await Task.Delay(TimeSpan.FromSeconds(0.7f));
             this.isJumping = false;
         }
@@ -106,12 +111,8 @@ namespace Controllers {
             return (!this.IsGrounded()) && Physics.Raycast(this.transform.position, Vector3.down, out hit, this.NEAR_GROUND_MARKER);
         }
 
-        private bool isFalling() {
-            return (!this.IsGrounded() && this.rigidbody.velocity.y < 0);
-        }
+        private bool IsFalling() => (!this.IsGrounded() && this.rigidbody.velocity.y < 0);
 
-        private void StartJumping() {
-            this.rigidbody.velocity = new Vector3(this.horizontalMoving, JUMP_VELOCITY, this.forwardMoving);
-        }
+        private void StartJumping() => this.rigidbody.velocity = new Vector3(this.horizontalMoving, JUMP_VELOCITY, this.forwardMoving);
     }
 }
