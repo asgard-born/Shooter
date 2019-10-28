e60eec4830bcd1c27d8a4d610ce9dcdf792416e9
Victor <unicaxverdad@gmail.com>
2019-10-28 17:09:02 +0300

    AIM IK Import

---
+++ Assets/Code/Structures/Weapon.cs
@@ -0,0 +1,13 @@
+﻿namespace Structures {
+    using UnityEngine;
+
+    public abstract class Weapon : MonoBehaviour {
+        public int        Id;
+        public string     weaponName;
+        public GameObject weaponType;
+        public int        damage;
+        public float      equipRate;
+        public float      holsterRate;
+        public AudioClip  attackSound;
+    }
+}
