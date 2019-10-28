f27ff02019cdc5a52f44d2a6b3fc2a088205c314
Victor <unicaxverdad@gmail.com>
2019-10-28 17:10:49 +0300

    Crosshair UI logic

---
+++ Assets/Code/UI/Crosshair.cs
@@ -0,0 +1,19 @@
+﻿namespace UI {
+    using UnityEngine;
+
+    public class Crosshair : MonoBehaviour {
+        [SerializeField] private Texture crosshairTexture;
+
+        private          Rect  crosshairPlace;
+        private readonly float crosshairSize      = Screen.width * 0.03f;
+        private readonly float crosshairRegulator = (Screen.height / 8);
+
+        private void OnGUI() {
+            this.crosshairPlace = new Rect(
+                Screen.width / 2                           - this.crosshairSize / 2,
+                Screen.height / 2 - this.crosshairSize / 2 - this.crosshairRegulator, this.crosshairSize, this.crosshairSize
+            );
+            GUI.DrawTexture(this.crosshairPlace, this.crosshairTexture);
+        }
+    }
+}
