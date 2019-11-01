2a1bda302670b8addbdac62dc5c7ce0ddf48d622
Victor <unicaxverdad@gmail.com>
2019-11-01 11:54:19 +0300

    grenade flow logic, refactor, fixes

---
+++ Assets/Code/Controllers/ArcRendererAlt.cs
@@ -0,0 +1,48 @@
+﻿namespace Controllers {
+    using UnityEngine;
+
+    public class ArcRendererAlt : MonoBehaviour {
+        private LineRenderer lineRenderer;
+
+        [SerializeField] private float velocity;
+        [SerializeField] private float angle;
+        [SerializeField] private int   pointsCount;
+
+        private float gravity;
+        private float radianAngle;
+
+        private void Awake() {
+            this.lineRenderer = this.GetComponent<LineRenderer>();
+            this.gravity      = Mathf.Abs(Physics.gravity.y);
+        }
+
+        private void Start() {
+            this.Render();
+        }
+
+        private void Render() {
+            this.lineRenderer.SetVertexCount(this.pointsCount);
+            this.lineRenderer.SetPositions(this.CalculateArcArray());
+        }
+
+        private Vector3[] CalculateArcArray() {
+            Vector3[] arcArray = new Vector3[this.pointsCount];
+
+            this.radianAngle = Mathf.Deg2Rad * this.angle;
+            float maxDistance = (this.velocity * this.velocity * Mathf.Sin(2 * this.radianAngle)) / this.gravity;
+
+            for (int i = 0; i < this.pointsCount; i++) {
+                float time = (float) i / (float) this.pointsCount;
+                arcArray[i] = this.CalculateArcPoint(time, maxDistance);
+            }
+
+            return arcArray;
+        }
+
+        private Vector3 CalculateArcPoint(float time, float maxDistance) {
+            float x = time * maxDistance;
+            float y = x * Mathf.Tan(this.radianAngle) - ((this.gravity * x * x) / (2 * this.velocity * this.velocity * Mathf.Cos(this.radianAngle) * Mathf.Cos(this.radianAngle)));
+            return new Vector3(x, y);
+        }
+    }
+}
