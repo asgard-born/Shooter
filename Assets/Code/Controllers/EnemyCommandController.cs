9728aaa8f848d14670d9db88a96a193cb45e6a9f
Victor <unicaxverdad@gmail.com>
2019-11-08 15:17:35 +0300

    + started to creating AI behaviour

---
+++ Assets/Code/Controllers/EnemyCommandController.cs
@@ -0,0 +1,62 @@
+﻿namespace Controllers {
+    using System;
+    using Interfaces;
+    using Structures;
+    using UnityEngine.AI;
+    using UnityEngine;
+
+    public class EnemyCommandController : MonoBehaviour, CommandController {
+        private InputController inputController;
+
+        public float ForwardMoving    { get; }
+        public float HorizontalMoving { get; }
+        public float RotateX          { get; }
+        public float RotateY          { get; }
+        public bool  IsSneak          { get; }
+        public bool  IsJumping        { get; }
+
+        public event Action OnFireOnce;
+        public event Action OnReload;
+        public event Action OnChangingWeapon;
+
+        private Transform PlayerT;
+        private AIPhase   phase;
+        private bool      needToSafeMyself;
+        private float     serialRate;
+        private float     distanceForAttack = 15;
+
+        public float SerialRate {
+            set => this.serialRate = value;
+        }
+
+        public void Initialize(InputController inputController) {
+        }
+
+        [SerializeField] private NavMeshAgent navMeshAgent;
+
+        public void Initialize(Transform playerT) {
+            this.PlayerT = playerT;
+        }
+
+        public void Update() {
+            if ((this.PlayerT.position - this.transform.position).magnitude <= this.distanceForAttack) {
+                this.phase = AIPhase.Attacking;
+            }
+            else {
+                this.phase = AIPhase.MovingToPlayer;
+            }
+
+            this.BehaveAccordingThePhase();
+        }
+
+        private void BehaveAccordingThePhase() {
+            if (this.phase == AIPhase.Attacking) {
+//                this.
+            }
+            else if (this.phase == AIPhase.MovingToPlayer) {
+            }
+            else if (this.phase == AIPhase.Dodging) {
+            }
+        }
+    }
+}
